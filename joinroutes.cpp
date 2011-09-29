285b63304ea6d2060494677d5b9aab30d5dbfadb
Marcelo Caggiani Luizelli <mcluizelli@gmail.com>
2011-09-29 16:05:48 -0300

    29-09-2011

--- joinroutes.cpp
+++ joinroutes.cpp
@@ -3,3 +3,42 @@
 JoinRoutes::JoinRoutes()
 {
 }
+
+void JoinRoutes::run(){
+
+    bool bFeasible    = false;
+    bool bTryFeasible = false;
+    bool bHasImprove  = true;
+
+    while(bHasImprove){
+
+        //Create possible moves based on iNumJoin
+        QList<> oListMoves = createMoves(iNumJoin);
+        //Sort oListMoves in decrease order (Based on function)
+        qSort(oListMoves.begin(), oListmoves.end(), compareFuncition);
+        //For each moves, try perform it until reach it.
+        for(int iContMove = 0; iContMove < oListMoves.size(); iContMove++){
+
+            Type oMove = oListMoves.at(iContMove);
+            //Try reach move.
+            bFeasible = tryMove(oMove);
+            //If the move is feasible, so its performed. Else, its activated a procedure that try turn feasible the move.
+            if(bFeasible){
+
+                performMove(oMove);
+
+            }else{
+
+                bTryFeasible = tryFeasible(oMove);
+                if(bTryFeasible){
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
--- joinroutes.h
+++ joinroutes.h
@@ -3,8 +3,12 @@
 
 class JoinRoutes
 {
+private:
+    void join();
+
 public:
     JoinRoutes();
+    void run();
 };
 
 #endif // JOINROUTES_H
